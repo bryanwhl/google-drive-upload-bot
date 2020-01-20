d6e40132144eacc96cce801be5c8a8d23d862bc8
Bryan Wong <bryanwhl1999@gmail.com>
2020-01-20 20:29:00 +0800

    final product

--- generate_new_file_id.py
+++ generate_new_file_id.py
@@ -1,3 +1,3 @@
 import requests
 
-requests.get('https://sbntech.pythonanywhere.com/create_new_folder604fa0fa-0f0b-47d9-8d0b-703e088cf5bb')
+requests.get('') #Insert URL for get request for generating new folder
